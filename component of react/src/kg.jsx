d4d3c6c212d485b8bdb44e1d5371ae217950b0b9
prabinnnn <prabinbhandari120@gmail.com>
2024-07-10 20:23:58 +0545

    react

---
+++ component of react/src/kg.jsx
@@ -0,0 +1,12 @@
+function home()
+{
+let myname='prabin'
+let fullname=()=>{
+    return 'prabin bhandari'
+}
+
+return <p>
+    hekdmfndnfdefbd  sicdnfndf kjdfnr{fullname()}
+</p>
+}
+export default home
